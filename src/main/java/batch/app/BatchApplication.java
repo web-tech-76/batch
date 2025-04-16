125bd7fc04ea8a0c1babc7c38a6f88f6f3b5f7be
shan09git <shanjob09@gmail.com>
2025-04-16 16:45:54 -0400

    new changes

--- src/main/java/batch/app/BatchApplication.java
+++ src/main/java/batch/app/BatchApplication.java
@@ -2,12 +2,29 @@ package batch.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@EnableScheduling
 @SpringBootApplication
 public class BatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchApplication.class, args);
-	}
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
+
+
+    @Component
+    class TaskExecute {
+
+        @Scheduled(fixedRate = 2000)
+        public void perform() {
+            System.out.println("print it every 2 seconds");
+        }
+
+    }
+
 
 }
